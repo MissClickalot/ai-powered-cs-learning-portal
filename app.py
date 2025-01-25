426f683203749ad171b3298ffd682fef702f08a6
missclickalot <evie.paige.anderson@gmail.com>
2025-01-25 17:49:33 +0000

    Update Flask routes in app.py.

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template, send_from_directory
 
 # Create an instance of the Flask class for the app
 app = Flask(__name__)
+
 # Define a route for the root URL
 @app.route('/')
-
 # Function to execute when user acesses the root URL
 def index():
     # Use the index.html template
     return render_template('index.html')
+
+# Define a route for each page
+@app.route('/<page>')
+def render_page(page):
+    try:
+        return render_template(f'{page}.html')
+    except:
+        return "Page not found", 404
 
 # Serve manifest.json from the static folder at the root URL
 @app.route('/manifest.json')
